66158a34ec7521a1e1e07c37f681d26bdd695e58
NarahariP <achitanarahari@gmail.com>
2020-05-10 19:57:23 +0530

    Data binding through alias names

--- AngularDataBinding/src/app/cockpit/cockpit.component.ts
+++ AngularDataBinding/src/app/cockpit/cockpit.component.ts
@@ -8,11 +8,11 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
   newServerName = '';
   newServerContent = '';
-  @Output() serverCreated = new EventEmitter<{
+  @Output('serverCreatedWithAlias') serverCreated = new EventEmitter<{
     name: string;
     content: string;
   }>();
-  @Output() blueprintCreated = new EventEmitter<{
+  @Output('blueprintCreatedWithAlias') blueprintCreated = new EventEmitter<{
     name: string;
     content: string;
   }>();
